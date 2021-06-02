2462d2f2afd3cc3ffa7ecaf1ca92a5721b45d1c5
Kody Deda <kodydeda4@gmail.com>
2021-06-02 11:30:15 -0400

    Initial Commit

---
+++ Todos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todos
+//
+//  Created by Kody Deda on 6/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Kody Deda on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
