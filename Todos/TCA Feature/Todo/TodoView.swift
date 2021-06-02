d37c1473c3be2100ad530bf7af0ff8334b6257b4
Kody Deda <kodydeda4@gmail.com>
2021-06-02 13:14:48 -0400

    fixed file structure

---
+++ Todos/TCA Feature/Todo/TodoView.swift
@@ -0,0 +1,39 @@
+//
+//  TodoView.swift
+//  Todos
+//
+//  Created by Kody Deda on 6/2/21.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct TodoView: View {
+    let store: Store<Todo.State, Todo.Action>
+    
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            VStack {
+                HStack {
+                    Button<Image>(viewStore.completed ? .largecircleFillCircle : .circle) {
+                        viewStore.send(.toggleCompleted)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .foregroundColor(.accentColor)
+                    
+                    TextField("Description", text: viewStore.binding(get: \.description, send: Todo.Action.updateText))
+                        .opacity(viewStore.completed ? 0.25 : 1)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                Divider()
+            }
+
+        }
+    }
+}
+
+struct TodoView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoView(store: Todo.defaultStore)
+    }
+}
---
+++ Todos/TCA Feature/TodosList/TodosListView.swift
@@ -0,0 +1,44 @@
+//
+//  TodosListView.swift
+//  Todos
+//
+//  Created by Kody Deda on 6/2/21.
+//
+
+
+import SwiftUI
+import ComposableArchitecture
+
+struct TodosListView: View {
+    let store: Store<TodosList.State, TodosList.Action>
+    
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            List {
+                ForEachStore(store.scope(
+                    state: \.todos,
+                    action: TodosList.Action.todos(index:action:)
+                ), content: TodoView.init)
+            }
+            .onAppear() {
+                viewStore.send(.onAppear)
+            }
+            .toolbar {
+                ToolbarItem {
+                    Button("Add") { viewStore.send(.createTodo) }
+                }
+                ToolbarItem {
+                    Button("Clear Completed") {
+                        viewStore.send(.clearCompleted)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct TodosListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodosListView(store: TodosList.defaultStore)
+    }
+}
